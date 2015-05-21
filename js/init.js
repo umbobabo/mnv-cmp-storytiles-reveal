eb4cb9465ae18b2de57bec95262e109a8d43b7cf
russEco <russstreet@economist.com>
2015-05-21 13:27:10 +0100

    init project

--- js/init.js
+++ js/init.js
@@ -15,6 +15,142 @@ $(document).ready(function(){
       $(this).html(tmp(widget.data));
     });
 
+
+
+$(document).ready(function(){
+  console.log('were ready');
+
+
+
+  var articleActive, revealContainer = document.getElementsByClassName('article-reveal-container'), btn = document.getElementsByClassName('close-btn'), tile = document.getElementsByClassName('artical-reveal-tile');
+
+
+  tileTarget =  $( ".article-reveal-container ul li:last-child" );
+  console.log(tileTarget);
+
+
+  tileTarget[0].addEventListener("transitionend", detectTheEnd, false);
+  tileTarget[0].addEventListener("webkitTransitionEnd", detectTheEnd, false);
+  tileTarget[0].addEventListener("mozTransitionEnd", detectTheEnd, false);
+  tileTarget[0].addEventListener("msTransitionEnd", detectTheEnd, false);
+  tileTarget[0].addEventListener("oTransitionEnd", detectTheEnd, false);
+
+
+  function detectTheEnd(e) {
+    if (e.propertyName == "opacity") {
+      console.log("Finished transition!" );
+      if(articleActive === true){
+        console.log(e);
+        // revealContainer[0].style.visibility = 'hidden';
+        // revealContainer[0].addClass('inactive');
+        revealContainer[0].classList.add('inactive');
+        btn[0].style.visibility = 'visible';
+        btn[0].classList.remove('inactive');
+      }
+    }
+  }
+
+  function triggerReverse(){
+   for (var i = 0; i <= revNumber.length; i++) {
+    $( ".artical-reveal-tile:eq(" + revNumber[i] + ")" ).toggleClass( "animate" ).css( "transition-delay", (0.05 *i) + "s" );
+  }
+}
+
+  function triggerForwards(){
+     for (var i = 0; i <= forNumber.length; i++) {
+    $( ".artical-reveal-tile:eq(" + forNumber[i] + ")" ).toggleClass( "animate" ).css( "transition-delay", (0.05 *i) + "s" );
+  }
+}
+
+
+
+
+    totalOfVals = $('.artical-reveal-tile').length;
+
+
+    $('.artical-reveal-tile').bind('click', function() {
+
+      $(this).toggleClass("animate");
+
+      articleActive = !articleActive;
+
+       if (articleActive === false){
+      //revealContainer[0].style.visibility = 'visible';
+      revealContainer[0].classList.add('active');
+    }
+
+
+      indexVal = $( ".artical-reveal-tile" ).index( $(this) );
+
+      revNumber = [];
+      forNumber = [];
+
+      indexValBelow = indexVal - 1;
+
+      for (var i = indexValBelow; i >= 0 ; i--) {
+        console.log("i from below: " +  i);
+        revNumber.push(i);
+      };
+
+      //loop through above and add animating class;
+      for (var i = (indexVal+1); i < totalOfVals; i++) {
+        forNumber.push(i);
+        console.log(forNumber);
+      };
+
+
+      triggerReverse();
+      triggerForwards();
+    });
+
+
+
+ // runAnim = function(){
+ //    articleActive = !articleActive;
+ //    if (articleActive === false){
+ //      revealContainer[0].style.visibility = 'visible';
+ //    }
+ //    // for (var i = 0; i <= (tile.length-1); i++) {
+ //    //   tile[i].classList.toggle('animate');
+ //    // };
+ // }
+
+
+
+
+
+  btn[0].addEventListener("click", function () {
+
+    this.classList.toggle('inactive');
+
+    // articleActive = !articleActive;
+
+
+    if (articleActive === true){
+      //revealContainer[0].style.visibility = 'visible';
+      revealContainer[0].classList.remove('inactive');
+      revealContainer[0].classList.add('active');
+      for (var i = 0; i <= (tile.length-1); i++) {
+      tile[i].classList.toggle('animate');
+    };
+
+    } else if(articleActive === false){
+      revealContainer[0].classList.remove('active');
+    }
+    console.log('articleActive: ' + articleActive)
+
+    articleActive = !articleActive;
+
+
+
+  });
+});
+
+
+
+
+    
+
   WLCMI.start();
 });
 
--- js/tpl/template.js
+++ js/tpl/template.js
@@ -6,13 +6,27 @@ templates['ec-storytilesreveal'] = template({"1":function(depth0,helpers,partial
   return "  <li>\n    "
     + this.escapeExpression(((helper = (helper = helpers.label || (depth0 != null ? depth0.label : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"label","hash":{},"data":data}) : helper)))
     + "\n  </li>\n";
+},"3":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "      <li class=\"artical-reveal-tile "
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "\">\n        <div class=\"tile-content\">\n          <h2 class=\"title\">"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h2>\n          <h2 class=\"rubrik\">"
+    + alias3(((helper = (helper = helpers.rubrik || (depth0 != null ? depth0.rubrik : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"rubrik","hash":{},"data":data}) : helper)))
+    + "</h2>\n          <img class=\"image\" src=\""
+    + alias3(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"image","hash":{},"data":data}) : helper)))
+    + "\">\n        </div>\n      </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper;
 
-  return "<p></p>\n<h1>"
+  return "<!-- <p></p>\n<h1>"
     + this.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h1>\n<ul>\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.list : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "</ul>";
+    + "</ul> -->\n\n<div class=\"main-container\">\n  <div class=\"close-btn\">X</div>\n  <div class=\"article-reveal-container\">\n    <ul>\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.tile : depth0),{"name":"each","hash":{},"fn":this.program(3, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "    </ul>\n  </div>\n</div>\n\n\n";
 },"useData":true});
 }());
