8f0e3afd3edcdb4f826fbaf5f46e9c49378a4a1b
umbobabo <umbertobabini@economist.com>
2015-06-17 12:43:35 +0100

    Change preloader with circle one

--- dist/js/init.js
+++ dist/js/init.js
@@ -4,12 +4,11 @@ function initReveal(){
   me = this,
   revealContainer = document.getElementsByClassName('article-reveal-container'),
   close = document.querySelector('.close-btn'),
-  tile = document.getElementsByClassName('article-reveal-tile'),
   articleContainer = document.querySelector('.article-container'),
 
   tileTarget =  document.querySelector( ".article-reveal-container .article-reveal-tile:last-child" );
   me.el = document.querySelector('.mnv-ec-storytilesreveal');
-  me.config = JSON.parse($(me.el).attr('data-config'));
+  me.config = JSON.parse(me.el.getAttribute('data-config'));
   me.pushStateUsed = false;
 
   tileTarget.addEventListener("transitionend", detectTheEnd, false);
@@ -18,37 +17,48 @@ function initReveal(){
   tileTarget.addEventListener("msTransitionEnd", detectTheEnd, false);
   tileTarget.addEventListener("oTransitionEnd", detectTheEnd, false);
 
-  preventDefaultClicks();
-
-  function preventDefaultClicks(){
-    $('.article-reveal-tile a, .close-btn').on('click', function(e){
-      e.preventDefault();
-    });
+ function preventDefaultClicks(e){
+    e.preventDefault();
   }
 
   function detectTheEnd(e) {
-    if ($(me.el).hasClass('landing')) {
-    //   if(articleActive !== true){
+    if (me.hasClass(me.el, 'landing')) {
       emptyArticle();
-      $(me.el).removeClass('animation-start').addClass('animation-end');
-    //   }
+      me.removeClass(me.el, 'animation-start');
+      me.addClass(me.el, 'animation-end');
     }
   }
 
+  me.hasClass = function (el, className){
+    if (el.classList)
+      return el.classList.contains(className);
+    else
+      return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+  }
+
   function loadArticle(html) {
-    //var tmp = Handlebars.templates['article'];
-    //var html = tmp(data);
-    $(articleContainer).html(html);
+    articleContainer.innerHTML = html;
     me.removePreloader();
     me.swapClass(true);
   }
 
-  $('.article-reveal-tile').bind('click', function() {
-    // TODO improve this part
-    var id = $(this).attr('data-id'), path = $('a', this).attr('href');
+  function handleArticleRequest() {
+    me.pushStateUsed = false;
+    var id = this.getAttribute('data-id'), path = this.children[0].getAttribute('href');
     me.showArticle(id, path);
-    $(me.el).removeClass('animation-end').addClass('animation-start');
-  });
+    me.removeClass(me.el, 'animation-end');
+    me.addClass(me.el, 'animation-start');
+  }
+
+  addEventListenerByClass('article-reveal-tile', 'click', handleArticleRequest);
+  addEventListenerByClass('article-reveal-tile-link', 'click', preventDefaultClicks);
+
+  function addEventListenerByClass(className, event, fn) {
+    var list = document.getElementsByClassName(className);
+    for (var i = 0, len = list.length; i < len; i++) {
+      list[i].addEventListener(event, fn, false);
+    }
+  }
 
   // Manage history changes
   window.onpopstate = function(event) {
@@ -60,16 +70,19 @@ function initReveal(){
     if(event.state.hasOwnProperty("articleID") && event.state){
       me.showArticle(event.state.articleID, event.state.articlePath);
     } else {
+      me.addClass(me.el, 'animation-start');
+      me.removeClass(me.el, 'animation-end');
       me.goLanding();
     }
-    this.pushStateUsed = false;
   };
 
   me.swapClass = function(article){
     if(article){
-      $(me.el).removeClass('landing').addClass('article');
+      me.removeClass(me.el, 'landing');
+      me.addClass(me.el, 'article');
     } else {
-      $(me.el).removeClass('article').addClass('landing');
+      me.removeClass(me.el, 'article');
+      me.addClass(me.el, 'landing');
     }
   }
 
@@ -79,42 +92,40 @@ function initReveal(){
     } else {
       // Preloader is inherited by Widget base class
       me.addPreloader();
-      $.get( path + "?output=html&template=partials/theWorldIf/article&data=http://" +  me.config.feed.host + "/" + me.config.feed.articlePath + "/" + articleID , function( html ) {
+      debugger;
+      me.ajax( path + "?output=html&template=partials/theWorldIf/article&data=http://" +  me.config.feed.host + "/" + me.config.feed.articlePath + "/" + articleID , function( html ) {
       // Olad approach with client-side handlebars and data request
-      //$.get("http://" +  me.config.feed.host + "/" + me.config.feed.articlePath + "/" + articleID + ".json", function( data ) {
+        //$.get("http://" +  me.config.feed.host + "/" + me.config.feed.articlePath + "/" + articleID + ".json", function( data ) {
         // Substitute inline element on the body
         //var parser = new inlineParser(data.body + 'ous east of the country, the post-Tiananmen [image|iid=86801|fid=165301|title=|alt=|caption=|use_original_size=|image_link=] deal\u2014stay out of politics and you can do anything you want\u2014is fraying, and [minerva|iid=87948|project=timeline] public outrage at corruption, pollution and other problems grows more vociferous. Yet rather than allow more formal popular participation and move towards the rule of law, China\u2019s leaders are allowing less participation as they crack down on free-thinkers, believing that carrying out real, structural reform is more dangerous than not doing so. In fact the opposite may be true. The deep fissures in the country will be increasingly hard to paper over with mere prosperity.\u003c\/p\u003e\u003cp\u003e\u003cspan\u003e[minerva|project=essay-china-gdp]\u003c\/span\u003e\u003c\/p\u003e\u003cp\u003eIt is not just that seeking to placate the public at home with \u003cem\u003ebraggadocio\u003c\/em\u003e overseas will make it harder still for China to garner allies and respect. There is a deeper problem. Many countries around the world admire, and would like to emulate, the undemocratic but effective way that China has managed its decades of growth. If China\u2019s domestic politics look less stable, some of that admiration will wane. And even if things can be held together, for the time being, admiration for China does not translate into affection for it, or');
         //var body = parser.parseHTML();
         loadArticle(html);
-      })
-      .fail(function() {
-        // TODO add error management
-      })
-      .done(function(data) {
-        // TODO complete this wit page information and do it DRY
         if(!me.pushStateUsed){
           customHistory.pushState({ articleID: articleID, articlePath: path },"Replace this with the title page", path);
         }
-      });
+      }, 'text');
     }
   }
 
   function emptyArticle(){
     articleContainer.innerHTML = '';
-    if(!me.pushStateUsed){
-      // TODO Replace title with page information
-      customHistory.pushState({},"Replace this with the title page", "/" + me.config.homeURL);
-    }
+    // if(!me.pushStateUsed){
+    //   // TODO Replace title with page information
+    //   customHistory.pushState({},"Replace this with the title page", "/" + me.config.homeURL);
+    // }
   }
 
   me.goLanding = function(){
-    customHistory.pushState({},"Replace this with the title page", "/" + me.config.homeURL);
+    if(!me.pushStateUsed){
+      customHistory.pushState({},"Replace this with the title page", "/" + me.config.homeURL);
+    }
     me.swapClass(false);
   }
 
   close.addEventListener("click", function () {
     me.goLanding();
   });
+  addEventListenerByClass('close-btn', 'click', preventDefaultClicks);
 
   return me;
 }
@@ -131,10 +142,17 @@ var customHistory = {
 
 docReady(function(){
   var reveal = new initReveal();
+  // Add FTScroller
+  containerElement = document.querySelector('.article-reveal-container');
+  scroller = new FTScroller(containerElement, {
+      scrollbars: false,
+      scrollingX: false
+  });
+  window.addEventListener('resize', scroller.updateDimensions, true);
   // Show article on final URL request
   // If an article is showed on load, wait a second and run the reveal animation
-  var articleID = $(reveal.el).attr("data-article-id"), articlePath = $(reveal.el).attr("data-article-path");
-  if($(reveal.el).hasClass('article')){
+  var articleID = reveal.el.getAttribute("data-article-id"), articlePath = reveal.el.getAttribute("data-article-path");
+  if(reveal.hasClass(reveal.el, 'article')){
     setTimeout(function(){
       reveal.showArticle(articleID, articlePath);
     }, 1500);
